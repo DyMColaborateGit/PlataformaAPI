6d1236be99d4f6f3d1f23d3f7698e2cb90df843b
DyMColaborateGit <git@dymsites.co>
2025-02-25 10:35:37 -0500

    Primer commit - 25-02-2025

---
+++ App.Logic/IServices/INivelDesempenoPpalService.cs
@@ -0,0 +1,22 @@
+﻿using App.Models.Models.TblCom;
+using System;
+using System.Threading.Tasks;
+
+namespace App.logic.IServices;
+
+public interface INivelDesempenoPpalService
+{
+    //Task<TBL_com_NivelesDesempenoPpalModels> GetObjNivelDesempenoPpal(int EmpresaId, string Nivel);
+    Task<List<TBL_com_NivelesDesempenoPpalModels>> GetListNivelDesempenoPpal(int EmpresaId, int InAnio);
+
+    Task<List<TBL_com_NivelesDesempenoPpalModels>> GetListConsolidadoNivelDesempeno(int EmpresaId, int InAnio, int ZonaId, int OficinaId, int ProcesoId, string EvaluadorId, long EvaluadoId);
+
+    Task<List<TBL_com_NivelesDesempenoPpalModels>> GetListConsolidadoNivelDesempenoM(int EmpresaId, int InAnio, int ZonaId, int OficinaId, int ProcesoId, string EvaluadorId, long EvaluadoId);
+
+    Task<List<ResponseTbl_com_ProgEvaluacionModels>> FiltroListConsolidadoNivelesDesempenoPpal(int EmpresaId, int UbicacionMD, int InAnio, int ZonaId, int OficinaId, string EvaluadorId, long EvaluadoId);
+
+    Task<List<ResponseTbl_com_ProgEvaluacionModels>> FiltroListConsolidadoNivelesDesempenoPpalM(int EmpresaId, int UbicacionMD_M, int InAnio, int ZonaId, int OficinaId, string EvaluadorId, long EvaluadoId);
+
+
+
+}
